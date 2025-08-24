7789f30286d12703ff8d3e18120d2841a95be442
asarkar6589 <sarkararnab266@gmail.com>
2025-08-24 21:49:30 +0530

    Reactive varibales, from & of operator

--- src/app/rxjx-basics/rxjx-basics.ts
+++ src/app/rxjx-basics/rxjx-basics.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjx-basics',
@@ -20,7 +20,23 @@ export class RxjxBasics {
     myObservable$.subscribe((res) => {
       console.log(res);
     });
+
+    this.cityList$.subscribe((res: string[]) => {
+      console.log(res);
+    });
+
+    // It will execute for 3 times.
+    this.cityList2$.subscribe((res: string) => {
+      console.log(res);
+    });
   }
 
-  
+  // Here we will be making a reactive variable. Let's say one eg of a non reactive variable.
+  cityList: string[] = ["koaa", "pune", "bombay"];
+
+  // Now if i want to make the above variable as reactive varibale, then we can make use of observable & store the data inside it. For that we have to use 'of' operator. Now cityList$ is an observable & we can subscribe it to get the data.
+  cityList$ = of(["koaa", "pune", "bombay"]); // it will emit all the data in one go
+
+  // now here i want to make a variable stateList$. But here i dont want all the data in one go. I want that whatever number of data i have in my variable, that many times my subscribe() function should execute. I want one by one element (just like a for loop)
+  cityList2$ = from(["koaa", "pune", "bombay"]); // it will emit the data one by one.
 }
